a986d5f6cf5030a8651f31058639d7ef8955e38c
ravi kumar <ravi kumar>
2017-01-12 11:59:29 +0530

    add serach component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,13 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var TodoAdd = require('TodoAdd');
+var TodoSearch = require('TodoSearch');
 //import FlatButton from 'material-ui/FlatButton';
 var TodoApp = React.createClass({
   getInitialState: function () {
     return{
+      showCompleted:false,
+      searchText: '',
       todos:[
         {
           id:1,
@@ -20,10 +23,17 @@ var TodoApp = React.createClass({
   handleTodoAdd: function(text){
     alert('new todo '+ text);
   },
+  handleSearch: function(showCompleted, searchText){
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
 render: function(){
   var {todos} =  this.state;
   return(
     <div>
+      <TodoSearch onSearch={this.handleSearch}/>
       <TodoList todos={todos}/>
       <TodoAdd onAddTodo={this.handleTodoAdd}/>
     </div>
