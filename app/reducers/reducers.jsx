0d4e0a476859d63122422edb413014fab99f2dda
ravi <ravi.kumar127@wipro.com>
2017-01-30 13:34:02 +0000

    reducer

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,61 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
+
+export var searchTextReducer = (state = '', action) => {
+
+  switch (action.type){
+    case 'SET_SEARCH_TEXT':
+    return action.searchText;
+
+    default : return state;
+  };
+};
+
+export var ShowCompletedReducer = (state = false, action) => {
+  switch (action.type){
+    case 'TOGGLE_SHOW_COMPLETED':
+    return !state;
+
+    default : return state;
+  };
+};
+
+export var addTodoReducer = (state = [], action) => {
+  switch (action.type){
+    case 'ADD_TODO':
+    return [
+      ...state, {
+        id: uuid(),
+        text:action.text,
+        completed: false,
+        createdAt: moment().unix(),
+        completedAt: undefined
+      }
+    ];
+
+//add case for toggleTodoReducer
+    case 'TOGGLE_TODO':
+    return state.map((todo) =>{
+      if(todo.id===action.id){
+        var nextCompleted =!todo.completed;
+        return {
+          ...todo,
+          completed: nextCompleted,
+          completedAt: nextCompleted ? moment().unix() : undefined
+        }
+      }
+    })
+
+    default : return state;
+  };
+};
+
+export var toggleTodoReducer = (state = '', action) => {
+  switch (action.type){
+    case 'TOGGLE_TODO':
+    return action.id;
+
+    default : return state;
+  };
+};
